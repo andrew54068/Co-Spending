51e8ff5191c678e364b4b1c9835718a21e539f8c
andrew54068 <andrew0424718012@yahoo.com.tw>
2022-10-30 14:25:35 +0800

    fix: redis connection.

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,7 +7,7 @@ import Redis
 
 // configures your application
 public func configure(_ app: Application) throws {
-    
+
     app.databases.use(.postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
@@ -15,35 +15,35 @@ public func configure(_ app: Application) throws {
         password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
         database: Environment.get("DATABASE_NAME") ?? "vapor_database"
     ), as: .psql)
-    
+
     app.migrations.add(CreateSpending(), to: DatabaseID.psql)
-    
+
     try app.autoMigrate().wait()
-    
-    let hostname = "127.0.0.1"
-    
+
+    let hostname = "redis"
+
     let config = try RedisConfiguration(
         url: "redis://\(hostname):6379",
         pool: .init(
-            maximumConnectionCount: .maximumPreservedConnections(2),
+            maximumConnectionCount: .maximumPreservedConnections(5),
             minimumConnectionCount: 1,
-            initialConnectionBackoffDelay: .seconds(1),
-            connectionRetryTimeout: .seconds(1)
+            initialConnectionBackoffDelay: .minutes(1),
+            connectionRetryTimeout: .minutes(1)
         )
     )
-    
+
     app.queues.use(
         .redis(
             config
         )
     )
-    
+
     // Register jobs
     let telegramBotJob = TelegramBotJob()
     app.queues.add(telegramBotJob)
-    
+
     try app.queues.startInProcessJobs()
-    
+
     // register routes
     try routes(app)
 }
